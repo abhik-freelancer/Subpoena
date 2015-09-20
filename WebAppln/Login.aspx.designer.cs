9948c7e1635689e79b0347939a366b7f9b1a2272
satasat999 <satyajit@thethinkerz.com>
2015-09-21 02:05:08 +0530

    Loading UI change

--- WebAppln/Login.aspx.designer.cs
+++ WebAppln/Login.aspx.designer.cs
@@ -83,14 +83,5 @@ namespace WebAppln {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btn_SignIn;
-        
-        /// <summary>
-        /// ForgotPassword_ModalPopupExtender control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::AjaxControlToolkit.ModalPopupExtender ForgotPassword_ModalPopupExtender;
     }
 }
